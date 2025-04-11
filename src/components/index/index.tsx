ee4dabf8849d03bde107bf4d852df758920cffea
marvin <sea-son@foxmail.com>
2025-04-11 16:28:30 +0800

    👌 IMPROVE: 样式优化

--- src/components/index/index.tsx
+++ src/components/index/index.tsx
@@ -4,26 +4,31 @@ export const Index = () => {
   const store = useAgentStore()
   return (
     <>
-      <div
-        onClick={() => {
-          updateMessage({ content: ' System Tips: click the btn!', uiId: UIId.system, id: Math.random().toString().slice(2) })
-        }}
-      >
-        push(system)
-      </div>
-      <div
-        onClick={() => {
-          updateMessage({ content: 'hello, can i help you?', uiId: UIId.user, id: Math.random().toString().slice(2) })
-        }}
-      >
-        push(user)
-      </div>
-      <div
-        onClick={() => {
-          updateMessage({ content: 'hello', uiId: UIId.bot, id: '27392189038218309' })
-        }}
-      >
-        update(bot)
+      <div className="flex gap-2 p-4">
+        <button
+          className="cursor-pointer bg-white border px-2 py-1 rounded hover:bg-slate-200"
+          onClick={() => {
+            updateMessage({ content: ' System Tips: click the btn!', uiId: UIId.system, id: Math.random().toString().slice(2) })
+          }}
+        >
+          push(system)
+        </button>
+        <button
+          className="cursor-pointer bg-white border px-2 py-1 rounded hover:bg-slate-200"
+          onClick={() => {
+            updateMessage({ content: 'hello, can i help you?', uiId: UIId.user, id: Math.random().toString().slice(2) })
+          }}
+        >
+          push(user)
+        </button>
+        <button
+          className="cursor-pointer bg-white border px-2 py-1 rounded hover:bg-slate-200"
+          onClick={() => {
+            updateMessage({ content: 'hello', uiId: UIId.bot, id: '27392189038218309' })
+          }}
+        >
+          update(bot)
+        </button>
       </div>
       {store.messages.map((item) => {
         const render = UIMap.get(item.uiId)
