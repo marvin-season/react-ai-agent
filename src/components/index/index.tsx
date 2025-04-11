74df7b6686538dd3e5028c9f5fb93729f5c63520
marvin <sea-son@foxmail.com>
2025-04-11 16:16:04 +0800

    👌 IMPROVE: 多角色消息实践

--- src/components/index/index.tsx
+++ src/components/index/index.tsx
@@ -6,17 +6,24 @@ export const Index = () => {
     <>
       <div
         onClick={() => {
-          updateMessage({ content: 'hello', uiId: UIId.user, id: Math.random().toString().slice(2) })
+          updateMessage({ content: ' System Tips: click the btn!', uiId: UIId.system, id: Math.random().toString().slice(2) })
         }}
       >
-        push
+        push(system)
       </div>
       <div
         onClick={() => {
-          updateMessage({ content: 'hello', uiId: UIId.user, id: '27392189038218309'})
+          updateMessage({ content: 'hello, can i help you?', uiId: UIId.user, id: Math.random().toString().slice(2) })
         }}
       >
-        update
+        push(user)
+      </div>
+      <div
+        onClick={() => {
+          updateMessage({ content: 'hello', uiId: UIId.bot, id: '27392189038218309' })
+        }}
+      >
+        update(bot)
       </div>
       {store.messages.map((item) => {
         const render = UIMap.get(item.uiId)
--- src/store/agent.tsx
+++ src/store/agent.tsx
@@ -7,29 +7,29 @@ export enum UIId {
 export const UIMap = new Map<UIId, (item: MessageProps) => ReactNode>()
 UIMap.set(UIId.system, (item) => {
   return (
-    <div>
-      <span>{'system'}</span>
-      <span>{item.id}</span>
-      <span>{item.content}</span>
+    <div className='bg-gray-300 rounded-md p-2 flex gap-2'>
+      <span className='bg-white px-2 py-1 rounded-xl text-sm'>{'system'}</span>
+      <span className='bg-blue-500 text-white px-2 py-1 rounded-xl text-sm'>{item.id}</span>
+      <span className='bg-green-400 text-white px-2 py-1 rounded-xl text-sm'>{item.content}</span>
     </div>
   )
 })
 UIMap.set(UIId.user, (item) => {
   return (
-    <div>
-      <span>{'user'}</span>
-      <span>{item.id}</span>
-      <span>{item.content}</span>
+    <div className='bg-gray-200 rounded-md p-2 flex gap-2'>
+      <span className='bg-white px-2 py-1 rounded-xl text-sm'>{'user'}</span>
+      <span className='bg-blue-400 text-white px-2 py-1 rounded-xl text-sm'>{item.id}</span>
+      <span className='bg-green-400 text-white px-2 py-1 rounded-xl text-sm'>{item.content}</span>
     </div>
   )
 })
 
 UIMap.set(UIId.bot, (item) => {
   return (
-    <div>
-      <span>{'bot'}</span>
-      <span>{item.id}</span>
-      <span>{item.content}</span>
+    <div className='bg-gray-100 rounded-md p-2 flex gap-2'>
+      <span className='bg-white px-2 py-1 rounded-xl text-sm'>{'bot'}</span>
+      <span className='bg-blue-300 text-white px-2 py-1 rounded-xl text-sm'>{item.id}</span>
+      <span className='bg-green-500 text-white px-2 py-1 rounded-xl text-sm'>{item.content}</span>
     </div>
   )
 })
