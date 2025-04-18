4de846b48600a8ec6a4908543d18e8075202ca14
marvin <sea-son@foxmail.com>
2025-04-18 11:09:42 +0800

    feat: icon

--- src/components/messages/Process.tsx
+++ src/components/messages/Process.tsx
@@ -1,4 +1,5 @@
 import { randomColorHex } from "@/utils/common"
+import { CircleCheckBig, Loader } from "lucide-react";
 import { memo } from "react"
 
 export enum ProcessStatus {
@@ -11,9 +12,15 @@ export interface ProcessProps {
     status: ProcessStatus;
     content: string;
 }
+
+const icons = {
+    [ProcessStatus.pending]: <Loader size={16} />,
+    [ProcessStatus.processing]: <Loader color="#4f4" size={16} />,
+    [ProcessStatus.completed]: <CircleCheckBig color="#4f7" size={16} />,
+}
 export const Process = memo(({ content, status }: ProcessProps) => {
-    return <div className="flex gap-2 border">
-        <span>{status}</span>
+    return <div className="flex gap-2 border mt-2 p-2 rounded bg-slate-50">
+        <span>{icons[status]}</span>
         <span style={{
             color: randomColorHex()
         }}>{content}</span>
