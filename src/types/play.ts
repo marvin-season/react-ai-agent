e0771e3c73d9c9f1325ae03ef02ed113f64d7c4a
marvin <sea-son@foxmail.com>
2025-04-18 11:49:48 +0800

    feat: ts 体操

---
+++ src/types/play.ts
@@ -0,0 +1,61 @@
+type ExtensionType = 'helloA' | 'helloB'
+
+export interface ExtensionConfig {
+    type: ExtensionType,
+    render: (...args: any[]) => any
+}
+
+const renderA = (props: { nameA: string, descriptionA?: string }) => {
+    return props.nameA
+}
+
+const extensionA = {
+    type: 'helloA' as const,
+    render: renderA,
+}
+
+const renderB = (props: { classnameB: string, descriptionB?: string }) => {
+    return props.classnameB
+}
+
+const extensionB = {
+    type: 'helloB',
+    render: renderB,
+} satisfies ExtensionConfig
+
+const extensions = [
+    extensionA,
+    extensionB,
+]
+
+export type Extension = (typeof extensions)[number]
+// TODO
+export function renderExtension<T extends ExtensionType>(options: {
+    type: T,
+    props: Parameters<Extract<Extension, { type: T }>['render']>[0]
+}) {
+}
+
+// 类型推导正常
+renderExtension({
+    type: 'helloA',
+    props: {
+        nameA: 'helloa'
+    }
+})
+
+// 类型推导正常
+renderExtension({
+    type: 'helloB',
+    props: {
+        classnameB: 'world'
+    }
+})
+
+// 类型推导异常
+renderExtension({
+    type: 'helloB',
+    props: {
+        classnameB: 'hello'
+    }
+})
